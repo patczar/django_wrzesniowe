28fd174bd1dc031337c2e2bec1db774c5abda031
Patryk z Linuxa <p.czarnik@alx.pl>
2025-09-14 10:07:53 +0200

    Definicja modelu w sklep

--- sklep/admin.py
+++ sklep/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+# from sklep.models import Product
+from .models import *
+
+admin.site.register(Product)
+
--- sklep/models.py
+++ sklep/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    vat = models.DecimalField(max_digits=2, decimal_places=2)
+    valid_to = models.DateField(blank=True, null=True)
+    available = models.BooleanField(default=True)
+
+    def vat_jako_calkowita(self):
+        return int(100 * self.vat)
+
